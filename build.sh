17c19f4a3650441194f85bb29a9c42203f7292b9
dragon <ExplodingFKL@gmail.com>
2023-10-06 01:11:51 +0800

    add

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-set -ex
+set -e
 git clone --depth=1 --branch=main https://github.com/armbian/build
 cd build
 ./compile.sh build BOARD=hinlink-h28k BRANCH=legacy BUILD_DESKTOP=no BUILD_MINIMAL=no EXPERT=yes KERNEL_CONFIGURE=no RELEASE=bookworm
+tar -cjvf /tmp/output.tar.bz2 output/images/*.img
