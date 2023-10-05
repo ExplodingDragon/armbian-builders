b70a6d2f8548dc1b8a384413f8026f0c3a6a3fb0
dragon <ExplodingFKL@gmail.com>
2023-10-06 01:30:43 +0800

    create small image

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -e
 git clone --depth=1 --branch=main https://github.com/armbian/build
 cd build
-./compile.sh build BOARD=hinlink-h28k BRANCH=legacy BUILD_DESKTOP=no BUILD_MINIMAL=no EXPERT=yes KERNEL_CONFIGURE=no RELEASE=bookworm
+/compile.sh build BOARD=hinlink-h28k BRANCH=legacy BUILD_DESKTOP=no BUILD_MINIMAL=yes EXPERT=no KERNEL_CONFIGURE=no RELEASE=bookworm INSTALL_HEADERS=yes BSPFREEZE=yes 
 tar -cjvf /tmp/output.tar.bz2 output/images/*.img
